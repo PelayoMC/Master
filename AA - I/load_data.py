d763bdc5315792bf6d44394125ed4bf70eef65b2
Pelayo <pmartinezcapela@gmail.com>
2022-11-05 15:04:06 +0100

    tema 2 aa

---
+++ AA - I/load_data.py
@@ -0,0 +1,22 @@
+from cmath import log
+import os
+import tarfile
+import pandas as pd
+import urllib.request as req
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    req.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    print('Datos descargados')
+    housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
