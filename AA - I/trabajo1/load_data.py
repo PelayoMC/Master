020b49d28027de55b3e80100c70789174f507306
Pelayo <pmartinezcapela@gmail.com>
2022-11-09 22:50:34 +0100

    bayes

--- AA - I/trabajo1/load_data.py
+++ AA - I/trabajo1/load_data.py
@@ -7,10 +7,10 @@ def load_data():
     csv_path = os.path.join(os.path.abspath(os.getcwd()), "airbnb.csv")
     return pd.read_csv(csv_path)
     
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    print('Datos separados')
-    return data.iloc[train_indices], data.iloc[test_indices]
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
