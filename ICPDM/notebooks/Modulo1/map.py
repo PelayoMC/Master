804a7fe156f687e54fe753b1dd8fc7a8c4569172
Pelayo <pmartinezcapela@gmail.com>
2022-11-13 21:18:24 +0100

    terminado aa empezado icm

---
+++ ICPDM/notebooks/Modulo1/map.py
@@ -0,0 +1,11 @@
+import sys
+
+#Leemos cada línea de la entrada estándar
+for line in sys.stdin:  
+  #Dividimos en palabras cada línea
+  words = line.split()  
+  #Iteramos sobre cada palabra contenida en words
+  for word in words:    
+  	#Escribimos por salida estándar el par <Clave,Valor>
+	#En este caso la Clave será la palabra en cuestión y el valor será 1
+    print(f"{word}\t1")
